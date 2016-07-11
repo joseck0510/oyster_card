01fee6aff6ae329988c72cf800b1a2f623c43e14
Jose <jcopoviking@outlook.com>
2016-07-11 20:42:29 +0100

    Adds Oystercard class with default balance of 0 at initialization of object.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,15 @@
 class Oystercard
+
+  DEFAULT_BALANCE = 0
+
+  attr_reader :balance
+
+  def initialize
+    balance = DEFAULT_BALANCE
+  end
+
+  def balance
+    balance = DEFAULT_BALANCE
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject( :card ) { Oystercard.new }
+
+  describe "the balance on a new oystercard"
+    it "checks the balance on the card is 0" do
+        expect(card.balance).to eq 0
+      end
 end
